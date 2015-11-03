55277e9fa6e04a5089a1e326409d18aacb1af8e3
andreasdjs <andreas.dagnell@gmail.com>
2015-11-03 14:42:18 +0100

    Add input field for username with jQuery

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+	var signup = 0;
+	$(".signup").click(function (event){
+		if (signup === 0) {
+			event.preventDefault();
+			$(".signin").before("<input type='username' placeholder='Username'/>");
+			signup = 1;
+		}
+	});
 });
