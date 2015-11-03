26e49d58d30d759d00886d5fcfbf03907188db1e
andreasdjs <andreas.dagnell@gmail.com>
2015-11-03 16:38:36 +0100

    Changed order of buttons back and added first- and last name. Also changed som jQuery.

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ $(document).ready(function(){
 	$(".signup").click(function (event){
 		if (signup === 0) {
 			event.preventDefault();
-			$(".submit").before("<input type='username' placeholder='Username'/>");
+//			$(".signin").before("<input type='name' placeholder='Firstname and lastname'>");
+			$("input:first-of-type").before("<input type='name' placeholder='Firstname and lastname'>");
+
+/*
+			$(".submit").fadeIn().before("<input type='username' placeholder='Username'/>").prev().hide().fadeIn("fast");
+		*/
 			signup = 1;
-			$(".signup").addClass("selected");
-			$(".signin").removeClass("selected");
+			$(".signin").remove();
+			$(".signup").after("<button class='signin'>Sign-In</button>");
+
+			$(".signup").addClass("selected"); 
+/*			$(".signin").removeClass("selected"); */
 		}
 	});
 });
+
+
+
+prev().hide().fadeIn(1000);
