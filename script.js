30e32c688dddd0b1076ae3eb59a2bd00d2c18eeb
andreasdjs <andreas.dagnell@gmail.com>
2015-11-09 17:28:37 +0100

    IE alert

--- script.js
+++ script.js
@@ -55,19 +55,18 @@ $(document).ready(function(){
 	});
 
 /* -- Defocus IE */
-
 function msieversion() {
-            var ua = window.navigator.userAgent;
-            var msie = ua.indexOf("MSIE ");
 
-            if (msie > 0)      // If Internet Explorer, return version number
-                alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
-            else                 // If another browser, return 0
-                alert('otherbrowser');
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
 
-            return false;
-        }
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
+            alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+        else                 // If another browser, return 0
+            alert('otherbrowser');
 
+   return false;
+}
 
 });
 
