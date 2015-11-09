45ab59a57a72bea6b252d9446aecd7aa4be7bd8b
andreasdjs <andreas.dagnell@gmail.com>
2015-11-09 17:34:46 +0100

    IE code

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function msieversion() {
 
         if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
             alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
-        else                 // If another browser, return 0
+        else {                // If another browser, return 0
        		$("input[type='email']").focus(); 
-/*            alert('otherbrowser'); */
+           alert('otherbrowser'); 
+		}
 
    return false;
 }
