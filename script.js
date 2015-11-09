609fee41bfcd47e4e771d0d18fb96a0e03e7dce3
andreasdjs <andreas.dagnell@gmail.com>
2015-11-09 17:28:06 +0100

    IE check

--- script.js
+++ script.js
@@ -54,5 +54,20 @@ $(document).ready(function(){
 		}, 1500);
 	});
 
+/* -- Defocus IE */
+
+function msieversion() {
+            var ua = window.navigator.userAgent;
+            var msie = ua.indexOf("MSIE ");
+
+            if (msie > 0)      // If Internet Explorer, return version number
+                alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+            else                 // If another browser, return 0
+                alert('otherbrowser');
+
+            return false;
+        }
+
+
 });
 
